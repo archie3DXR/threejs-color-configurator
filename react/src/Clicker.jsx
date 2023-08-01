af5dd20f063b228232c888e2a38ae26e280c3a53
Archana Majumdar <archie.majumdar@gmail.com>
2023-08-01 16:15:10 +1000

    incorporate useEffect and localStorage

--- react/src/Clicker.jsx
+++ react/src/Clicker.jsx
@@ -1,21 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react"
 
 export default function Clicker() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(
+    parseInt(localStorage.getItem("count") ?? 0)
+  )
 
- 
+  useEffect(() => {}, [])
+
+  useEffect(() => {
+    localStorage.setItem("count", count)
+  }, [count])
+
   const buttonClick = () => {
-    setCount(count + 1);
-  };
+    setCount(count + 1)
+  }
+
   const buttonClock = () => {
-    setCount(count - 2);
-  };
+    setCount(count - 2)
+  }
+
+  const buttonReset = () => {
+    setCount(0)
+  }
+
   return (
     <div>
       <div>number: {count} </div>
       <button onClick={buttonClick}>add 1</button>
       <button onClick={buttonClock}>minus 2</button>
-      <button onClick={() => setCount(0)}>reset</button>
+      <button onClick={buttonReset}>reset</button>
     </div>
-  );
+  )
 }
